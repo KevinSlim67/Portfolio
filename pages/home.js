15d0e0e0470beea5eb55af6e6758e9494474188c
KevinSlim67 <kevinslim167@gmail.com>
2022-09-16 18:30:53 +0300

    Contact Form made functionnal

--- pages/home.js
+++ pages/home.js
@@ -1,20 +1,13 @@
-function highlightAll(event) {
-    const className = event.classList[1].replace("-demo", "");
-    const elements = document.getElementsByClassName(className);
-    const bgColor = getComputedStyle(event).backgroundColor;
-
-    for (e in elements) {
-        if (elements[e].style !== undefined) elements[e].style.backgroundColor = bgColor;
-    }
-}
-
-function unHighlightAll(event) {
-    const className = event.classList[1].replace("-demo", "");
-    const elements = document.getElementsByClassName(className);
-
-    const bgColor = "rgba(255, 255, 255, 0.103)";
-
-    for (e in elements) {
-        if (elements[e].style !== undefined) elements[e].style.backgroundColor = bgColor;
-    }
+function sendEmail() {
+    Email.send({
+        Host : "smtp.yourisp.com",
+        Username : "username",
+        Password : "password",
+        To : 'them@website.com',
+        From : "you@isp.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
 }
