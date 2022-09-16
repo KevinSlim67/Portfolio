8ee57638fd6fd908c55ddd4049205691710c39d6
KevinSlim67 <kevinslim167@gmail.com>
2022-09-16 18:37:43 +0300

    fixed nav paths

--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,26 @@
+function highlightAll(event) {
+    const className = event.classList[1].replace("-demo", "");
+    const elements = document.getElementsByClassName(className);
+    const bgColor = getComputedStyle(event).backgroundColor;
+
+    for (e in elements) {
+        if (elements[e].style !== undefined) elements[e].style.backgroundColor = bgColor;
+    }
+}
+
+function unHighlightAll(event) {
+    const className = event.classList[1].replace("-demo", "");
+    const elements = document.getElementsByClassName(className);
+
+    const bgColor = "rgba(255, 255, 255, 0.103)";
+
+    for (e in elements) {
+        if (elements[e].style !== undefined) elements[e].style.backgroundColor = bgColor;
+    }
+}
 function sendEmail() {
     Email.send({
-        Host : "smtp.yourisp.com",
+        Host : "smtp.gmail.com",
         Username : "username",
         Password : "password",
         To : 'them@website.com',
